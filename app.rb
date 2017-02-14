6f44eedd29e3423d9031546ff43db94146b7fc92
xgironx <xgironx@gmail.com>
2017-02-14 15:12:43 -0500

    ran Beautify trying to diagnose why i cant see a specific pokemon

--- app.rb
+++ app.rb
@@ -1,61 +1,60 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
-require "pry"
+require 'pry'
 
-# Load the file to connect to the DB
+							# Load the file to connect to the DB
 require_relative 'db/connection.rb'
 
-# # Load specific routes / controllers
-# require_relative 'controllers/pokemon.rb'
-# require_relative 'controllers/songs.rb'
+							# # Load specific routes / controllers
+							# require_relative 'controllers/pokemon.rb'
+							# require_relative 'controllers/songs.rb'
 
-# Load models
+							# Load models
 require_relative 'models/pokemon'
-# require_relative 'models/song'
+							# require_relative 'models/song'
 
 get '/pokemon' do
-  @pokemon = Pokemon.all
-  erb :"pokemon/index"
+    @pokemon = Pokemon.all
+    erb :"pokemon/index"
 end
-              ################################################################_GIRO
-              # ORDER COUNTS, THIS HAS TO BE IN FRONT OF THE post, create new
-              # THAT YOU SEE DOWN AROUND LINE #37 BELOW
-              # FOR SOME REASON I DIDNT UNDERSTAND
-get '/pokemon/new' do
-  @pokemon = Pokemon.all
-  erb :"pokemon/new"
-end           ################################################################_GIRO
-
-              ################################################################_GIRO
-              # DONT USE TRAILING SLASH, NO WORKEE
+							# ###############################################################_GIRO
+							# DONT USE TRAILING SLASH, NO WORK
 get '/pokemon/:id' do
-  @artist = Pokemon.find(params[:id])
-  # @pokemon = Pokemon.find(params[:id])
-  erb :"pokemon/show"
-end           ################################################################_GIRO
+    @artist = Pokemon.find(params[:id])
+    # @pokemon = Pokemon.find(params[:id])
+    erb :"pokemon/show"
+end           # ###############################################################_GIRO
 
+							# ###############################################################_GIRO
+							# ORDER COUNTS, THIS HAS TO BE IN FRONT OF THE post, create new
+							# THAT YOU SEE DOWN AROUND LINE #37 BELOW
+							# FOR SOME REASON I DIDNT UNDERSTAND
+get '/pokemon/new' do
+    @pokemon = Pokemon.all
+    erb :"pokemon/new"
+end           # ###############################################################_GIRO
 
-# post '/pokemon' do
-#   @artist = Pokemon.create(:artist)  # THIS IS THE 'artist hash referred to at
-#                                     # https://github.com/ga-wdi-lessons/sinatra-and-activerecord
-#   # @artist = Pokemon.create(name: params[:name],   age:  params:[:age])
-#   redirect 'pokemon/#{@artist.id}'
-# end
-#
-# put '/pokemon' do                   # THIS IS THE 'artist hash referred to at
-#   @artist = Pokemon.find(params[:id])
-#   @artist = Pokemon.update[:artist]  # THIS IS THE 'artist hash referred to at
-#                                     # https://github.com/ga-wdi-lessons/sinatra-and-activerecord
-#   redirect 'pokemon/#{@artist.id}'
-# end
-#
-# delete '/pokemon/:id' do
-#   @artist = Pokemon.find(params[:id])
-#   @artist.destroy
-#   redirect("/pokemon")
-# end
+							# post '/pokemon' do
+							#   @artist = Pokemon.create(:artist)  # THIS IS THE 'artist hash referred to at
+							#                                     # https://github.com/ga-wdi-lessons/sinatra-and-activerecord
+							#   # @artist = Pokemon.create(name: params[:name],   age:  params:[:age])
+							#   redirect 'pokemon/#{@artist.id}'
+							# end
+							#
+							# put '/pokemon' do                   # THIS IS THE 'artist hash referred to at
+							#   @artist = Pokemon.find(params[:id])
+							#   @artist = Pokemon.update[:artist]  # THIS IS THE 'artist hash referred to at
+							#                                     # https://github.com/ga-wdi-lessons/sinatra-and-activerecord
+							#   redirect 'pokemon/#{@artist.id}'
+							# end
+							#
+							# delete '/pokemon/:id' do
+							#   @artist = Pokemon.find(params[:id])
+							#   @artist.destroy
+							#   redirect("/pokemon")
+							# end
 
-puts "end of application"
+puts 'end of application'
 
-# binding.pry
+							# binding.pry
--- db/connection.rb
+++ db/connection.rb
@@ -1,18 +1,16 @@
 # Load Active Record and connect to the DB
-ActiveRecord::Base.establish_connection({
-  adapter: 'postgresql',
-  database: 'pokemon_db'
-})
+ActiveRecord::Base.establish_connection(adapter: 'postgresql',
+                                        database: 'pokemon_db')
 
-#  Alternate syntax:
-# ActiveRecord::Base.establish_connection(
-#   :database => 'pokemon_db',
-#   :adapter => 'postgresql'
-# )
+                  #  Alternate syntax:
+                  # ActiveRecord::Base.establish_connection(
+                  #   :database => 'pokemon_db',
+                  #   :adapter => 'postgresql'
+                  # )
 
-# Fix an issue with sinatra and Active Record where connections are left open
+                  # Fix an issue with sinatra and Active Record where connections are left open
 if defined? Sinatra
-	after do
-	  ActiveRecord::Base.connection.close
-	end
+    after do
+        ActiveRecord::Base.connection.close
+    end
 end
